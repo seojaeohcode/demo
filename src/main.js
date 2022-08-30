3e2e2761092d2590e97c2ae657c93603dff06254
seojaeoh <seojaeohcode@gmail.com>
2022-08-30 10:46:39 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import status from './components/status'
+import newPage from './components/newPage'
+// 전역으로 등록
+const app = createApp(App);
+app.component('status',status);
+app.component('newPage',newPage);
+
+app.use(store).use(router).mount('#app')
